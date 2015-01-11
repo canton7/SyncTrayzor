e56e2fb13e1cfe74d71cee766d85821c5ce51669
Antony Male <antony.male@gmail.com>
2015-01-11 18:04:51 +0000

    Shell application

---
+++ src/SyncTrayzor/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Stylet;
+using SyncTrayzor.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTrayzor
+{
+    public class Bootstrapper : Bootstrapper<ShellViewModel>
+    {
+    }
+}
