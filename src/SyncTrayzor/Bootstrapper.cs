2eb840f58ae5789ebebe175750f7b0edd2e2099d
Antony Male <antony.male@gmail.com>
2015-01-12 13:19:10 +0000

    Minor tweaks, and gearing up for the API client

--- src/SyncTrayzor/Bootstrapper.cs
+++ src/SyncTrayzor/Bootstrapper.cs
@@ -15,11 +15,12 @@ namespace SyncTrayzor
         protected override void ConfigureIoC(IStyletIoCBuilder builder)
         {
             builder.Bind<ISyncThingRunner>().To<SyncThingRunner>().InSingletonScope();
+            builder.Bind<ISyncThingClient>().To<SyncThingClient>().InSingletonScope();
         }
 
         protected override void OnExit(System.Windows.ExitEventArgs e)
         {
-            this.Container.Get<ISyncThingRunner>().Dispose();
+            this.Container.Dispose();
         }
     }
 }
---
+++ src/SyncTrayzor/Services/SyncThingClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTrayzor.Services
+{
+    public interface ISyncThingClient
+    {
+
+    }
+
+    public class SyncThingClient : ISyncThingClient
+    {
+
+    }
+}
