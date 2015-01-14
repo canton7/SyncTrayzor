9791dab49efeffd242f6c86f7ec50bfffc797c8b
Antony Male <antony.male@gmail.com>
2015-01-14 13:24:31 +0000

    Minor WIP Tweaks

--- src/SyncTrayzor/Xaml/WebBrowserUtilities.cs
+++ src/SyncTrayzor/Xaml/WebBrowserUtilities.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SyncTrayzor.Xaml
 {
     public static class WebBrowserUtilities
     {
+        private static readonly Guid sid_webBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+
         public static string GetLocation(DependencyObject obj)
         {
             return (string)obj.GetValue(LocationProperty);
@@ -29,6 +32,20 @@ namespace SyncTrayzor.Xaml
                     webBrowser.Navigate((string)e.NewValue);
             }));
 
+
+        public static readonly RoutedEvent ExternalWindowOpenedEvent = EventManager.RegisterRoutedEvent("ExternalWindowOpened", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(WebBrowserUtilities));
+        public static void AddExternalWindowOpenedHandler(DependencyObject d, RoutedEventHandler handler)
+        {
+            UIElement uie = d as UIElement;
+            if (uie != null)
+                uie.AddHandler(WebBrowserUtilities.ExternalWindowOpenedEvent, handler);
+        }
+        public static void RemoveExternalWindowOpenedHandler(DependencyObject d, RoutedEventHandler handler)
+        {
+            UIElement uie = d as UIElement;
+            if (uie != null)
+                uie.RemoveHandler(WebBrowserUtilities.ExternalWindowOpenedEvent, handler);
+        }
         
     }
 }
