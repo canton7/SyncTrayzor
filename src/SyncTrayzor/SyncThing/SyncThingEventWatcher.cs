966d6431b8d720656b729149e9135455c59c9564
Antony Male <antony.male@gmail.com>
2015-02-07 12:15:28 +0000

    Abstract api polling functionality into its own base class

--- src/SyncTrayzor/SyncThing/SyncThingEventWatcher.cs
+++ src/SyncTrayzor/SyncThing/SyncThingEventWatcher.cs
@@ -9,88 +9,58 @@ using System.Threading.Tasks;
 
 namespace SyncTrayzor.SyncThing
 {
-    public interface ISyncThingEventWatcher
+    public interface ISyncThingEventWatcher : ISyncThingPoller
     {
-        bool Running { get; set; }
         event EventHandler<SyncStateChangedEventArgs> SyncStateChanged;
         event EventHandler StartupComplete;
     }
 
-    public class SyncThingEventWatcher : ISyncThingEventWatcher, IEventVisitor
+    public class SyncThingEventWatcher : SyncThingPoller, ISyncThingEventWatcher, IEventVisitor
     {
         private readonly ISyncThingApiClient apiClient;
 
         private int lastEventId;
 
-        private bool _running;
-        public bool Running
-        {
-            get { return this._running; }
-            set
-            {
-                if (this._running == value)
-                    return;
-
-                this._running = value;
-                if (value)
-                {
-                    this.Start();
-                }
-            }
-        }
-
         public event EventHandler<SyncStateChangedEventArgs> SyncStateChanged;
         public event EventHandler StartupComplete;
 
         public SyncThingEventWatcher(ISyncThingApiClient apiClient)
+            : base(TimeSpan.Zero)
         {
             this.apiClient = apiClient;
         }
 
-        private async void Start()
+        protected override void Start()
         {
             this.lastEventId = 0;
+            base.Start();
+        }
+
+        protected override async Task PollAsync()
+        {
             try
             {
-                while (this._running)
+                List<Event> events;
+                // If we don't know what the latest event ID is (disconnection? new connection?), make sure we find out
+                if (this.lastEventId == 0)
+                    events = await this.apiClient.FetchEventsAsync(0, 1);
+                else
+                    events = await this.apiClient.FetchEventsAsync(this.lastEventId);
+
+                foreach (var evt in events)
                 {
-                    bool errored = false;
-
-                    try
-                    {
-                        List<Event> events;
-                        // If we don't know what the latest event ID is (disconnection? new connection?), make sure we find out
-                        if (this.lastEventId == 0)
-                            events = await this.apiClient.FetchEventsAsync(0, 1);
-                        else
-                            events = await this.apiClient.FetchEventsAsync(this.lastEventId);
-
-                        foreach (var evt in events)
-                        {
-                            this.lastEventId = Math.Max(this.lastEventId, evt.Id);
-                            System.Diagnostics.Debug.WriteLine(evt);
-                            evt.Visit(this);
-                        }
-                    }
-                    catch (HttpRequestException)
-                    {
-                        errored = true;
-                    }
-                    catch (IOException)
-                    {
-                        // Socket forcibly closed. Could be a restart, could be a termination. We'll have to continue and quit if we're stopped
-                        // A restart means the lastEventId will be reset
-                        this.lastEventId = 0;
-                        errored = true;
-                    }
-
-                    if (errored)
-                        await Task.Delay(1000);
+                    this.lastEventId = Math.Max(this.lastEventId, evt.Id);
+                    System.Diagnostics.Debug.WriteLine(evt);
+                    evt.Visit(this);
                 }
             }
-            finally
+            catch (IOException)
             {
-                this._running = false;
+                // A restart means the lastEventId will be reset
+                this.lastEventId = 0;
+
+                // Need the base method to do the error handling
+                throw; 
             }
         }
 
