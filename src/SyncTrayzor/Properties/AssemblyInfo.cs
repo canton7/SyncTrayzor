126fa37bc1a0f6238ac21b6124033be317080e1a
Antony Male <antony.male@gmail.com>
2015-02-08 17:09:52 +0000

    Update copyright

--- src/SyncTrayzor/Properties/AssemblyInfo.cs
+++ src/SyncTrayzor/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SyncTrayzor")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Antony Male 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
