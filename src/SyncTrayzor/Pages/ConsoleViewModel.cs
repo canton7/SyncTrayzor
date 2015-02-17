85f1f915772c8ce11bc2c197241415ee7bf393ee
Antony Male <antony.male@gmail.com>
2015-02-17 13:17:44 +0000

    Handle lots of fast log messages better

--- src/SyncTrayzor/Pages/ConsoleViewModel.cs
+++ src/SyncTrayzor/Pages/ConsoleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Stylet;
 using SyncTrayzor.SyncThing;
+using SyncTrayzor.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,23 @@ namespace SyncTrayzor.Pages
 {
     public class ConsoleViewModel : Screen
     {
+        private const int maxLogMessages = 500;
+
         private readonly ISyncThingManager syncThingManager;
 
-        public string LogMessages { get; private set; }
+        public ObservableQueue<string> LogMessages { get; private set; }
 
         public ConsoleViewModel(ISyncThingManager syncThingManager)
         {
             this.syncThingManager = syncThingManager;
-            this.LogMessages = "";
+            this.LogMessages = new ObservableQueue<string>();
 
-            // TODO: UGLY!
-            this.syncThingManager.MessageLogged += (o, e) => Execute.OnUIThread(() => this.LogMessages += e.LogMessage + "\n");
+            this.syncThingManager.MessageLogged += (o, e) =>
+            {
+                this.LogMessages.Enqueue(e.LogMessage);
+                if (this.LogMessages.Count > maxLogMessages)
+                    this.LogMessages.Dequeue();
+            };
         }
     }
 }
---
+++ src/SyncTrayzor/Utils/ObservableQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTrayzor.Utils
+{
+    public class ObservableQueue<T> : IEnumerable<T>, INotifyCollectionChanged
+    {
+        private readonly Queue<T> queue = new Queue<T>();
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public int Count
+        {
+            get { return this.queue.Count; }
+        }
+
+        public void Enqueue(T item)
+        {
+            this.queue.Enqueue(item);
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, this.queue.Count - 1));
+        }
+
+        public T Dequeue()
+        {
+            T item = this.queue.Dequeue();
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, 0));
+            return item;
+        }
+
+        private void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            var handler = this.CollectionChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.queue.GetEnumerator();
+        }
+    }
+}
