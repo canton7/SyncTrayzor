23b7d2c2ae36776e4fd87f2b3c38588ac628cf4e
Adrian Rudnik <adrian.rudnik@gmail.com>
2015-03-09 16:51:11 +0100

    Added option to obfuscate device IDs in the console/log view

--- src/SyncTrayzor/Pages/ConsoleViewModel.cs
+++ src/SyncTrayzor/Pages/ConsoleViewModel.cs
@@ -1,11 +1,13 @@
-﻿using Stylet;
+using Stylet;
 using SyncTrayzor.SyncThing;
 using SyncTrayzor.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using SyncTrayzor.Services;
 
 namespace SyncTrayzor.Pages
 {
@@ -17,14 +19,28 @@ namespace SyncTrayzor.Pages
 
         public ObservableQueue<string> LogMessages { get; private set; }
 
-        public ConsoleViewModel(ISyncThingManager syncThingManager)
+        public ConsoleViewModel(
+            ISyncThingManager syncThingManager,
+            IConfigurationProvider configurationProvider
+            )
         {
+            var configuration = configurationProvider.Load();
+
             this.syncThingManager = syncThingManager;
             this.LogMessages = new ObservableQueue<string>();
 
             this.syncThingManager.MessageLogged += (o, e) =>
             {
-                this.LogMessages.Enqueue(e.LogMessage);
+                var message = e.LogMessage;
+
+                // Check if device IDs need to be obfuscated
+                if (configuration.ObfuscateDeviceIDs)
+                {
+                    var rgx = new Regex(@"-[0-9A-Z]{7}-[0-9A-Z]{7}-[0-9A-Z]{7}-[0-9A-Z]{7}-[0-9A-Z]{7}-[0-9A-Z]{7}-[0-9A-Z]{7}");
+                    message = rgx.Replace(message, "");
+                }
+
+                this.LogMessages.Enqueue(message);
                 if (this.LogMessages.Count > maxLogMessages)
                     this.LogMessages.Dequeue();
             };
