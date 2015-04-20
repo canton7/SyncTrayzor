2475b102f8051c64483031f9e5254b175bc15774
Antony Male <antony.male@gmail.com>
2015-04-20 12:01:23 +0100

    Escape underscores in folders in icon context menu
    Fixes #58

---
+++ src/SyncTrayzor/Xaml/RemoveMnemonicsConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SyncTrayzor.Xaml
+{
+    public class RemoveMnemonicsConverter : IValueConverter
+    {
+        public static RemoveMnemonicsConverter Instance = new RemoveMnemonicsConverter();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var val = value as string;
+            if (val == null)
+                return null;
+
+            return val.Replace("_", "__");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var val = value as string;
+            if (val == null)
+                return null;
+
+            return val.Replace("__", "_");
+        }
+    }
+}
