c9954d816acad1554a0f5e5e6fa12d6294e0c89c
Antony Male <antony.male@gmail.com>
2015-06-16 11:15:34 +0100

    Update events

--- src/SyncTrayzor/SyncThing/ApiClient/StartupCompleteEvent.cs
+++ src/SyncTrayzor/SyncThing/ApiClient/StartupCompleteEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace SyncTrayzor.SyncThing.ApiClient
 {
     public class StartupCompleteEvent : Event
     {
+        [JsonProperty("myID")]
+        public string MyID { get; set; }
+
         public override void Visit(IEventVisitor visitor)
         {
             visitor.Accept(this);
