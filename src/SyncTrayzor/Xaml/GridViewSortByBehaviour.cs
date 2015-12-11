f9c62f1c2a2c6c8b6686f863d90253451575ab1d
Antony Male <antony.male@gmail.com>
2015-12-11 13:40:07 +0000

    Start working on an attached behaviour to handle sorting

---
+++ src/SyncTrayzor/Xaml/GridViewSortByBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SyncTrayzor.Xaml
+{
+    public class GridViewSortByBehaviour : DetachingBehaviour<ListView>
+    {
+        protected override void AttachHandlers()
+        {
+            this.AssociatedObject.AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(this.GridViewColumnHeaderClicked));
+        }
+
+        protected override void DetachHandlers()
+        {
+            this.AssociatedObject.RemoveHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(this.GridViewColumnHeaderClicked));
+        }
+
+        private void GridViewColumnHeaderClicked(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            if (headerClicked == null)
+                return;
+
+            this.SortBy("FileName");
+        }
+
+        private void SortBy(string propertyName)
+        {
+            var collectionView = CollectionViewSource.GetDefaultView(this.AssociatedObject.ItemsSource);
+            collectionView.SortDescriptions.Clear();
+            collectionView.SortDescriptions.Add(new SortDescription(propertyName, ListSortDirection.Descending));
+            collectionView.Refresh();
+        }
+    }
+}
