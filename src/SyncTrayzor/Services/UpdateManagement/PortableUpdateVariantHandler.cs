283736186dfbd8f13a8513cac675918c9a02abf2
Antony Male <antony.male@gmail.com>
2016-02-10 17:33:35 +0000

    Fix portable upgrades where there's a space in the path
    Or, it seems, where SyncTrayzor was minimized when the upgrade prompt
    occurred, although that hasn't been reported as an error.
    
    Fixes #232

--- src/SyncTrayzor/Services/UpdateManagement/PortableUpdateVariantHandler.cs
+++ src/SyncTrayzor/Services/UpdateManagement/PortableUpdateVariantHandler.cs
@@ -94,7 +94,9 @@ namespace SyncTrayzor.Services.UpdateManagement
 
             var pid = Process.GetCurrentProcess().Id;
 
-            var args = $"\"{Path.GetDirectoryName(this.assemblyProvider.Location)}\" \"{this.extractedZipPath}\" {pid} \"{pathToRestartApplication}\"";
+            // pathToRestartApplication IS ALREADY QUOTED: it's `"C:\Foo\Bar.exe"` or `"C:\Foo\Bar.exe" --minimized`. Portable installer
+            // knows to look for either 4 or 5 arguments to take account of the fact that pathToRestartApplication may contain two bits 
+            var args = $"\"{Path.GetDirectoryName(this.assemblyProvider.Location)}\" \"{this.extractedZipPath}\" {pid} {pathToRestartApplication}";
 
             this.processStartProvider.StartDetached(destPortableInstaller, args);
 
