29a54be946433db82b1da9ca0015be89804ed4d2
Antony Male <antony.male@gmail.com>
2017-04-06 14:54:29 +0100

    Update DotNetVersionFinder

--- src/SyncTrayzor/Utils/DotNetVersionFinder.cs
+++ src/SyncTrayzor/Utils/DotNetVersionFinder.cs
@@ -12,12 +12,15 @@ namespace SyncTrayzor.Utils
         {
             { 378389, "4.5" },
             { 378675, "4.5.1 on Windows 8.1 or Windows Server 2012 R2" },
-            { 378758, "4.5.1 on WIndows 8, Wwndows 7 SPI1, or Windows Vista SP2" },
+            { 378758, "4.5.1 on WIndows 8, Windows 7 SPI1, or Windows Vista SP2" },
             { 379893, "4.5.2" },
             { 393295, "4.6 on Windows 10" },
-            { 393297, "4.6 on all other OS versions" },
+            { 393297, "4.6 on non-Windows 10" },
             { 394254, "4.6.1 on Windows 10 November Update systems" },
-            { 394271, "4.6.1 on all other OS versions" },
+            { 394271, "4.6.1 on non-Windows 10 November Update systems" },
+            { 394802, "4.6.2 on Windows 10 Anniversary Update systems" },
+            { 394806, "4.6.2 on non-Windows 10 Anniversary Update systems" },
+            { 460798, "4.7 on Windows 10 Creators Update systems" },
         };
 
         public static string FindDotNetVersion()
