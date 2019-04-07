3ccc5e62f2c8477f20887a04eadc8b385f3b5ee5
Antony Male <antony.male@gmail.com>
2019-04-07 15:49:33 +0100

    Fix version_check.php

--- server/version_check.php
+++ server/version_check.php
@@ -82,7 +82,7 @@ $versions = [
       'sha1sum_download_url' => "{base_url}/v{version}/sha1sum.txt.asc",
       'sha512sum_download_url' => "{base_url}/v{version}/sha512sum.txt.asc",
       'release_page_url' => 'https://github.com/canton7/SyncTrayzor/releases/tag/v{version}',
-      'release_notes' => "- Fix some crashes (#443, #463, #468, #471)\n- Show the folder name, not ID, in the file transfers progress tooltip  (#457)\n- Add tray button to rescan all folders (#465)\n- Show change in Syncthing theme without requiring refresh (#472)\n- Allow STTRACE to be set in env vars in settings (#473)\n- Fix slow memory leak (#479)",
+      'release_notes' => "- Show 'modified by' device in conflict resolver (#492)\n- Fix possible cause of null bytes in config file (#471)\n- If creating the first folder, enable notifications by default (#495)",
    ],
    '1.1.21' => [
       'base_url' => 'https://synctrayzor.antonymale.co.uk/download',
