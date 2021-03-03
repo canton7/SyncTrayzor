57822b6f15f3096e4d5b9a9a54297479cea27de6
Antony Male <antony.male@gmail.com>
2021-03-03 21:06:41 +0000

    Show a bar alert when Xe graphics are detected
    Relates to: #606

---
+++ src/SyncTrayzor/Services/GraphicsCardDetector.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+using System.Management;
+
+namespace SyncTrayzor.Services
+{
+    public class GraphicsCardDetector
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private bool? _isIntelXe;
+        public bool IsIntelXe
+        {
+            get
+            {
+                if (this._isIntelXe == null)
+                    this._isIntelXe = GetIsIntelXe();
+                return this._isIntelXe.Value;
+            }
+        }
+
+        private static bool GetIsIntelXe()
+        { 
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                if (obj["CurrentBitsPerPixel"] != null && obj["CurrentHorizontalResolution"] != null)
+                {
+                    string name = obj["Name"]?.ToString();
+                    if (name.IndexOf("Intel", StringComparison.OrdinalIgnoreCase) >= 0 &&
+                        name.IndexOf(" Xe ", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        logger.Info($"Graphics card: {name}");
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
